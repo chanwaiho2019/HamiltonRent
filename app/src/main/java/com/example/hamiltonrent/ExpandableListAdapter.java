df81e372cab7013f1e23457e71f316682f11d7e7
chanwaiho <waihochan2019@gmail.com>
2020-01-18 13:31:09 +1300

    Updated the HarcourtsWebScraper methods (getHamiltonRentResidentialData(), getByBedroomNum() and findNumberOfPages()).

--- app/src/main/java/com/example/hamiltonrent/ExpandableListAdapter.java
+++ app/src/main/java/com/example/hamiltonrent/ExpandableListAdapter.java
@@ -1,6 +1,8 @@
 package com.example.hamiltonrent;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +11,7 @@ import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.gargoylesoftware.htmlunit.javascript.host.dom.Text;
 
 import java.util.HashMap;
 import java.util.List;
@@ -88,21 +90,63 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         textViewItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (textViewItem.getText().equals(context.getResources().getString(R.string.oneBedroom))){
-                    Toast.makeText(context, getGroup(i) + " : 1 bedroom clicked" , Toast.LENGTH_SHORT ).show();
+                //The sublist of Harcourts is selected
+                if (getGroup(i).equals(context.getResources().getString(R.string.harcourts))) {
+                    if (isOneBedroom(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 1 bedroom clicked", Toast.LENGTH_SHORT).show();
+                    } else if (isTwoBedrooms(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 2 bedrooms clicked", Toast.LENGTH_SHORT).show();
+                    } else if (isThreeBedrooms(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 3 bedrooms clicked", Toast.LENGTH_SHORT).show();
+                    } else if (isFourBedrooms(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 4 bedrooms clicked", Toast.LENGTH_SHORT).show();
+                    } else if (isFiveBedrooms(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 5 bedrooms clicked", Toast.LENGTH_SHORT).show();
+                    }
                 }
-                else if (textViewItem.getText().equals(context.getResources().getString(R.string.twoBedroom))){
-                    Toast.makeText(context, getGroup(i) + " : 2 bedrooms clicked" , Toast.LENGTH_SHORT ).show();
+                //The sublist of Waikato Real Estate is selected
+                else if (getGroup(i).equals(context.getResources().getString(R.string.waikatoRealEstate))){
+                    if (isOneBedroom(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 1 bedroom clicked", Toast.LENGTH_SHORT).show();
+                    } else if (isTwoBedrooms(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 2 bedrooms clicked", Toast.LENGTH_SHORT).show();
+                    } else if (isThreeBedrooms(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 3 bedrooms clicked", Toast.LENGTH_SHORT).show();
+                    } else if (isFourBedrooms(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 4 bedrooms clicked", Toast.LENGTH_SHORT).show();
+                    } else if (isFiveBedrooms(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 5 bedrooms clicked", Toast.LENGTH_SHORT).show();
+                    }
                 }
-                else if (textViewItem.getText().equals(context.getResources().getString(R.string.threeBedroom))){
-                    Toast.makeText(context, getGroup(i) + " : 3 bedrooms clicked" , Toast.LENGTH_SHORT ).show();
+                //The sublist of Lodge is selected
+                else if (getGroup(i).equals(context.getResources().getString(R.string.lodge))){
+                    if (isOneBedroom(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 1 bedroom clicked", Toast.LENGTH_SHORT).show();
+                    } else if (isTwoBedrooms(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 2 bedrooms clicked", Toast.LENGTH_SHORT).show();
+                    } else if (isThreeBedrooms(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 3 bedrooms clicked", Toast.LENGTH_SHORT).show();
+                    } else if (isFourBedrooms(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 4 bedrooms clicked", Toast.LENGTH_SHORT).show();
+                    } else if (isFiveBedrooms(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 5 bedrooms clicked", Toast.LENGTH_SHORT).show();
+                    }
                 }
-                else if (textViewItem.getText().equals(context.getResources().getString(R.string.fourBedroom))){
-                    Toast.makeText(context, getGroup(i) + " : 4 bedrooms clicked" , Toast.LENGTH_SHORT ).show();
+                //The sublist of RayWhite is selected
+                else if (getGroup(i).equals(context.getResources().getString(R.string.rayWhite))){
+                    if (isOneBedroom(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 1 bedroom clicked", Toast.LENGTH_SHORT).show();
+                    } else if (isTwoBedrooms(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 2 bedrooms clicked", Toast.LENGTH_SHORT).show();
+                    } else if (isThreeBedrooms(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 3 bedrooms clicked", Toast.LENGTH_SHORT).show();
+                    } else if (isFourBedrooms(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 4 bedrooms clicked", Toast.LENGTH_SHORT).show();
+                    } else if (isFiveBedrooms(textViewItem)) {
+                        Toast.makeText(context, getGroup(i) + " : 5 bedrooms clicked", Toast.LENGTH_SHORT).show();
+                    }
                 }
-                else{
-                    Toast.makeText(context, getGroup(i) + " : 5 bedrooms clicked" , Toast.LENGTH_SHORT ).show();
-                }
+
             }
         });
         return view;
@@ -111,5 +155,50 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
     @Override
     public boolean isChildSelectable(int i, int i1) {
         return false;
+    }
+
+    /**
+     * A method to check the text of a textView is "1 bedroom"
+     * @param textView The TextView to be checked
+     * @return True if the text is "1 bedroom", false otherwise
+     */
+    public boolean isOneBedroom(TextView textView){
+        return textView.getText().equals(context.getResources().getString(R.string.oneBedroom));
+    }
+
+    /**
+     * A method to check the text of a textView is "2 bedrooms"
+     * @param textView The TextView to be checked
+     * @return True if the text is "2 bedrooms", false otherwise
+     */
+    public boolean isTwoBedrooms(TextView textView){
+        return textView.getText().equals(context.getResources().getString(R.string.twoBedroom));
+    }
+
+    /**
+     * A method to check the text of a textView is "3 bedrooms"
+     * @param textView The TextView to be checked
+     * @return True if the text is "3 bedrooms", false otherwise
+     */
+    public boolean isThreeBedrooms(TextView textView){
+        return textView.getText().equals(context.getResources().getString(R.string.threeBedroom));
+    }
+
+    /**
+     * A method to check the text of a textView is "4 bedrooms"
+     * @param textView The TextView to be checked
+     * @return True if the text is "4 bedrooms", false otherwise
+     */
+    public boolean isFourBedrooms(TextView textView){
+        return textView.getText().equals(context.getResources().getString(R.string.fourBedroom));
+    }
+
+    /**
+     * A method to check the text of a textView is "5 bedrooms"
+     * @param textView The TextView to be checked
+     * @return True if the text is "5 bedrooms", false otherwise
+     */
+    public boolean isFiveBedrooms(TextView textView){
+        return textView.getText().equals(context.getResources().getString(R.string.fiveBedroom));
     }
 }
--- app/src/main/java/com/example/hamiltonrent/HarcourtsWebScraper.java
+++ app/src/main/java/com/example/hamiltonrent/HarcourtsWebScraper.java
@@ -12,9 +12,22 @@ import java.util.concurrent.TimeUnit;
 
 public class HarcourtsWebScraper {
 
+    //The URL of Harcourts (Rent - Residential)
+    public static final String url = "https://harcourts.co.nz/Property/Rentals?location=25015&minbed=1&data=%7B%22region%22%3A22003%2C%22city%22%3A25015%7D";
+
     //The web driver used for scraping
     public static WebDriver driver = null;
 
+    /**
+     * A method to get all the residential properties for rent from Harcourts website
+     * @return A list of residential properties.
+     *         It is a list of String[], in which the format is as follow:
+     *         String[0]: Title of the property
+     *         String[1]: Address of the property
+     *         String[2]: The rent(per week) of the property
+     *         String[3]: The number of bedrooms for rent
+     *         String[4]: The link for detail description of the property
+     */
     public List<String[]> getHamiltonRentResidentialData() {
         //Allow access the memory location of chrome driver
         System.setProperty("webdriver.chrome.driver", "app/libs/chromedriver");
@@ -28,27 +41,77 @@ public class HarcourtsWebScraper {
         //Give it 15 seconds to load
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         //Connect to the url
-        driver.get("https://harcourts.co.nz/Property/Rentals?location=25015&data=%7B%22region%22%3A22003%2C%22city%22%3A25015%7D");
+        driver.get(url);
+        //Find the number of pages that it has
+        int numOfPages = findNumberOfPages(url);
+        //System.out.println(numOfPages);
 
         //Store the data in variables (Rent - Residential)
         String title;
         String address;
-        String cost;
+        String rent;
+        String numOfBedroom;
         String link;
         List<String[]> list = new ArrayList<>();
 
-
-        //Locate web elements
-        List<WebElement> webElements = driver.findElements(By.cssSelector("div.search-item-content"));
-        for (WebElement we : webElements){
-            title = we.findElement(By.cssSelector("a")).getText();
-            address = we.findElement(By.cssSelector("address")).getText();
-            cost = we.findElement(By.cssSelector("p span")).getText();
-            link = we.findElement(By.cssSelector("a")).getAttribute("href");
-            list.add(new String[]{title, address, cost, link});
-            //list.add(title + " " + address + " " + cost + " " + link);
+        for (int i = 1; i <= numOfPages; i++){
+            //Locate web elements
+            List<WebElement> webElements = driver.findElements(By.cssSelector("div.search-item-content"));
+            for (WebElement we : webElements){
+                title = we.findElement(By.cssSelector("a")).getText();
+                address = we.findElement(By.cssSelector("address")).getText();
+                rent = we.findElement(By.cssSelector("p span")).getText();
+                numOfBedroom = we.findElement(By.cssSelector("span.hc-text")).getText();
+                link = we.findElement(By.cssSelector("a")).getAttribute("href");
+                list.add(new String[]{title, address, rent, numOfBedroom, link});
+                //list.add(title + " " + address + " " + cost + " " + link);
+            }
+            //Locate the web element for the url of next page
+            WebElement nextPage = driver.findElement(By.cssSelector("div.next-pagination a"));
+            //Get the url of next page
+            String nextURL = nextPage.getAttribute("href");
+            //Navigate to the next page
+            driver.get(nextURL);
         }
+        //System.out.println(list.size());    //Print out the number of search results
         return list;
+    }
+
+    /**
+     * A method to find the total number of pages for result
+     * @param url The url to check
+     * @return The total number of pages
+     */
+    public static int findNumberOfPages(String url){
+        //Navigate to the original url
+        driver.get(url);
+        int num = 0;
+        List<WebElement> webElements = driver.findElements(By.cssSelector("div.page-list a"));
+        for (WebElement we : webElements){
+            try{
+                num = Integer.parseInt(we.getText());
+            }
+            catch (NumberFormatException e){
+                e.printStackTrace();
+            }
+        }
+        return num;
+    }
+
+    /**
+     * Get the list of properties by specified number of bedrooms
+     * @param list The full list of properties
+     * @param bedroomNum The number of bedrooms that you want to specify
+     * @return The list of properties with the number of bedrooms that you specified
+     */
+    public List<String[]> getByBedroomNum(List<String[]> list, int bedroomNum){
+        List<String[]> temp = new ArrayList<>();
+        for (String[] property : list){
+            if (Integer.parseInt(property[3]) == bedroomNum){
+                temp.add(property);
+            }
+        }
+        return temp;
     }
 
     public static void main(String[] args) {
@@ -58,8 +121,10 @@ public class HarcourtsWebScraper {
             System.out.println(s[0]);
             System.out.println(s[1]);
             System.out.println(s[2]);
-            System.out.println(s[3]);
+            System.out.println("Number of bedrooms: " + s[3]);
+            System.out.println(s[4]);
             System.out.println("---------------");
         }
+
     }
 }
